227936ed373f37c19c03c4adf4edb2f90ebacf41
willleong805850 <58291874+willleong805850@users.noreply.github.com>
2020-05-24 17:11:57 +0800

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,70 @@ const errors = require('restify-errors');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../model/User');
+const Items = require('../model/Items');
 const auth = require('../auth');
 const config = require('../config');
 
 
 
 module.exports = server => {
+
+    //Get all user
+
+    server.get('/user', async(req,res, next) => {
+        try{
+            const users = await User.find({});
+            res.send(users);
+            next();
+
+        }catch(err){
+            return next(new errors.InvalidContentError(err));
+        }
+        
+
+    });
+
+    //Get Single Users
+
+    server.get('/user/:id', async(req, res, next) => {
+        try{    
+            const items = await User.findById(req.params.id);
+            res.send(items);
+            next();
+        }catch(err){
+         return next(new errors.ResourceNotFoundError(`There is no customer with the id of ${req.params.id}`));
+        }
+        });
+
+     //Update Users
+    
+     server.put('/user/:id', async(req, res, next) => {
+        //Check for JSON
+        if(!req.is('application/json')){
+            return next(new errors.InvalidContentError("Expects 'application/json'"));
+        }
+        
+        try{
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(req.body.password, salt, async (err, hash) => {
+                   //Hash password
+                   req.body.password = hash;
+                   //Save User
+                   try{
+                    const item = await User.findOneAndUpdate({ _id: req.params.id }, req.body);
+                    res.send(200);
+                    next();
+                   }catch(err){
+                       return next(new errors.InternalError(err.message));
+                   }
+                });       
+               });
+            
+        }catch{
+            return next(new errors.ResourceNotFoundError(`There is no item with the id of ${req.params.id}`)); 
+        }
+    });
+
     //Register User
     server.post('/register', (req, res, next) => {
         const{email, password} = req.body;
@@ -53,6 +111,18 @@ module.exports = server => {
         }catch(err){
             //User unauthorized
             return next(new errors.UnauthorizedError(err));
+        }
+    });
+
+    //Delete User
+
+    server.del('/user/:id', async(req, res, next) =>{
+        try{
+            const items = await User.findOneAndRemove({_id: req.params.id});
+            res.send(204);
+            next();
+        }catch{
+            return next(new errors.ResourceNotFoundError(`There is no item with the id of ${req.params.id}`)); 
         }
     });
 };
